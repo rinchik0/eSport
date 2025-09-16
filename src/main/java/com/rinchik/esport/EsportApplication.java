3dd8a1f116c0bf1c713dac4205f8a9cb58f94bb9
rinchik0 <ariadna.kolupaeva@yandex.ru>
2025-09-16 20:32:21 +0300

    Initial commit

---
+++ src/main/java/com/rinchik/esport/EsportApplication.java
@@ -0,0 +1,13 @@
+package com.rinchik.esport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rinchik/esport/EsportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rinchik.esport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
